922b775c3e9d7c2901874cd0d0acaea452c527c2
Riccardo Conci <r.conci@etononline.net>
2019-06-20 16:29:10 -0400

    added visualisation of testing data

--- Architectures/VAEs/visuals.py
+++ Architectures/VAEs/visuals.py
@@ -4,14 +4,18 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
+import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf
+plt.rcParams['figure.figsize'] = [15, 15]
+
+
+
 class traverse_z():
-    def __init__(self, NN):
+    def __init__(self, NN, num_frames = 20):
         #super(traverse_z, self).__init__()
         self.z_dim = NN.z_dim
         self.NN = NN
         
-        
-    def create_plots(num_frames = 20):
         self.num_slice = int(1000/num_frames)
         self.num_frames = num_frames
         orig_sample = 0 #torch.randn(self.z_dim)
@@ -28,8 +32,6 @@ class traverse_z():
             
         reconst = self.NN._decode(traverse_input)
 
-        
-    def create_gif():
         indexs = np.arange(0, self.num_frames*self.z_dim, self.z_dim)
         for i in indexs:
             images = []
@@ -47,3 +49,48 @@ class traverse_z():
             if not os.path.exists(directory_2):
                     os.makedirs(directory_2)
             imageio.mimsave('traversals_gifs/traversing_z_{}.gif'.format(int(i/self.z_dim),int(i/self.z_dim)), images)
+            
+            
+            
+class plotsave_tests():
+    def __init__(self, NN, MyDataset, test_image_paths, test_target_paths, pdf_path, n=20):
+        ## NN: neural network class
+        ## MyDataset: Image loader class
+        ## test_image_paths: location of testing x image
+        ## test_target_paths: location of testing y image
+        ## pdf_path: location of where to save pdf 
+        ## n : number of testing images reconstruct and save
+        
+        self.NN = NN
+        self.MyDataset = MyDataset
+        self.test_image_paths = test_image_paths
+        self.test_target_paths = test_target_paths
+        self.pdf_path = pdf_path
+        self.n = n
+    
+    
+        dset = self.MyDataset
+        test_data = dset(self.test_image_paths,self.test_target_paths, image_size= 64)
+
+        pdf = matplotlib.backends.backend_pdf.PdfPages(self.pdf_path)
+
+        for i in range(self.n):
+            x, y = test_data.__getitem__(i)
+            x = torch.unsqueeze(x, 0)
+            #x = Variable(cuda(x, self.use_cuda))
+            x_recon, _, _ = self.NN(x)
+            x = x.detach().numpy()
+            y = y.detach().numpy()
+            x_recon = x_recon.detach().numpy()
+
+            f, (a0, a1, a2) = plt.subplots(1, 3, gridspec_kw={'width_ratios': [1, 1,1]})
+            a0.imshow(x[0,0,:,:])
+            a1.imshow(y[0,:,:])
+            a2.imshow(x_recon[0,0,:,:])
+            f.tight_layout()
+            pdf.savefig(f, dpi=300)
+            plt.close()
+
+        pdf.close()
+
+    
