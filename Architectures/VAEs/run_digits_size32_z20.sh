aed3cda432e1eae7f2436d1345e127f2926e2a2e
Riccardo Conci <r.conci@etononline.net>
2019-06-25 20:56:42 -0400

    changes to nfilter

--- Architectures/VAEs/run_digits_size32_z20.sh
+++ Architectures/VAEs/run_digits_size32_z20.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-python main_mod.py --train True --ckpt_name last --image_size 32 --model conv_VAE_32 --z_dim 20 \
+python main_mod.py --train True --ckpt_name last --image_size 32 --model conv_VAE_32 \
+    --z_dim 20 --n_filter 32 \
     --max_iter 4800 --gather_step 10 --display_step 20 \
-    --dset_dir /Users/riccardoconci/Desktop/2_dig_fixed_random_bw/digts/ \
+    --dset_dir /home/riccardo/Desktop/Digts_Exp_1/digts/ \
     --batch_size 128 --lr 5e-4 --beta 1 \
 
